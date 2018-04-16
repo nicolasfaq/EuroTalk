246cda7d928eb0a65bd0d67ed1428b6c6d2a4da8
Azerka <nicolas.faqir@hotmail.fr>
2018-04-16 17:08:34 +0200

    First Commit

---
+++ EuroTalk/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EuroTalk.Startup))]
+namespace EuroTalk
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
